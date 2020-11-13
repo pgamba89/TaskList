68bc25cc9d441315585053ac09516b64c675a42d
Pablo Gamba <pablogamba@outlook.com>
2020-11-13 12:42:53 +0000

    ViewModelInject on DetailModel. Refactor

--- app/src/main/java/com/example/taskstodo/taskDetail/TaskDetailFragment.kt
+++ app/src/main/java/com/example/taskstodo/taskDetail/TaskDetailFragment.kt
@@ -6,15 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import com.example.taskstodo.R
 import com.example.taskstodo.databinding.FragmentTaskDetailBinding
 import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class TaskDetailFragment : Fragment() {
 
-    private lateinit var modelView: TaskDetailModelView
-    private lateinit var taskDetailModelViewFactory: TaskDetailModelViewFactory
+    private val modelView: TaskDetailModelView by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,13 +25,6 @@ class TaskDetailFragment : Fragment() {
             inflater, R.layout.fragment_task_detail, container, false
         )
 
-        val application = requireNotNull(this.activity).application
-        val args = TaskDetailFragmentArgs.fromBundle(requireArguments())
-
-        taskDetailModelViewFactory = TaskDetailModelViewFactory(args.taskId, application)
-
-        modelView =
-            ViewModelProvider(this, taskDetailModelViewFactory).get(TaskDetailModelView::class.java)
         binding.viewModel = modelView
         binding.lifecycleOwner = this
 
